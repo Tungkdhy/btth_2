798d96a3825b0fd4fdd02ad48dc9188bc5af74b9
Your Name <you@example.com>
2025-07-31 13:36:26 +0700

    [Add] sub component

---
+++ src/app/modules/dasboard-t5/components/left-panel/components/mangqs/stack-chart-hori/stack-chart-hori.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { StackChartComponent } from './stack-chart-hori.component';
+
+describe('StackChartComponent', () => {
+  let component: StackChartComponent;
+  let fixture: ComponentFixture<StackChartComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [StackChartComponent]
+    });
+    fixture = TestBed.createComponent(StackChartComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
